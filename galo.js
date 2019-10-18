7945f1078ce02dab569ea81bbe0d0fc24f697771
Marcelo Garcia <margarcia@zendesk.com>
2019-10-18 12:00:32 +1100

    split galo

---
+++ galo.js
@@ -0,0 +1,61 @@
+
+const logEnabled = (process.env["LOG"] == "true");
+
+let pilha = [];
+let threshold = 5;
+let timeWindowMillisecs = 10000;
+
+const agora = () => {
+    return new Date().getTime();
+}
+
+const pruneStack = () => {
+    while (pilha.length > threshold) {
+        pilha.shift();
+    }
+}
+
+const cantaGalo = () => {
+    pilha.push(agora());
+    pruneStack();
+}
+
+const lowerBound = () => {
+    return agora() - timeWindowMillisecs;
+}
+
+const isCallWithinWindow = (call) => {
+    return call >= lowerBound();
+}
+
+const isStackFull = () => !!pilha[threshold -1]
+    
+const shouldCanta = () => isStackFull() && isCallWithinWindow(pilha[0])
+
+const messages = [
+    "Da Rooster will sing for you",
+    "A rooster announces to a flock of chickens that he’s found food with a “took, took, took.” But the hens don’t pay attention if they already know that there is food around.",
+    "which came first, the chicken or the egg?",
+    "The Universe Is Old (Really Old)",
+    "The Universe Has Echoes of Its Birth, think about it",
+    "Mind you, roosters dont sing, they crow.",
+    "The chicken was the first bird to have its genome sequenced, in 2004.",
+    "Baby chickens are chicks. Female chickens are pullets until they’re old enough to lay eggs and become hens. Male chickens are called roosters, cocks or cockerels, depending on the country you’re in"
+]
+
+const niceMessage = () => {
+    const mId = Math.floor(Math.random() * Math.floor(8));
+    return messages[mId];
+}
+
+module.exports.galoGET = (req, res) => res.send(shouldCanta()? "canta" : "cala");
+
+module.exports.galoPOST = (req, res) => {
+    cantaGalo()
+    if (logEnabled) {
+        console.log("req.headers", req.headers);
+        console.log("req.params", req.params);
+        console.log("req.body", req.body);
+    }
+    res.send(200, niceMessage())
+}
--- server.js
+++ server.js
@@ -1,67 +1,13 @@
 const express = require('express')
+const galo = require('./galo')
 const app = express()
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env["PORT"] || 8080
-const logEnabled = (process.env["LOG"] == "true");
-let pilha = [];
-let threshold = 5;
-let timeWindowMillisecs = 10000;
 
-const agora = () => {
-    return new Date().getTime();
-}
+app.get('/canta-galo', galo.galoGET)
+app.post('/canta-galo', galo.galoPOST)
 
-const pruneStack = () => {
-    while (pilha.length > threshold) {
-        pilha.shift();
-    }
-}
-
-const cantaGalo = () => {
-    pilha.push(agora());
-    pruneStack();
-}
-
-const lowerBound = () => {
-    return agora() - timeWindowMillisecs;
-}
-
-const isCallWithinWindow = (call) => {
-    return call >= lowerBound();
-}
-
-const isStackFull = () => !!pilha[threshold -1]
-    
-const shouldCanta = () => isStackFull() && isCallWithinWindow(pilha[0])
-
-const messages = [
-    "Da Rooster will sing for you",
-    "A rooster announces to a flock of chickens that he’s found food with a “took, took, took.” But the hens don’t pay attention if they already know that there is food around.",
-    "which came first, the chicken or the egg?",
-    "The Universe Is Old (Really Old)",
-    "The Universe Has Echoes of Its Birth, think about it",
-    "Mind you, roosters dont sing, they crow.",
-    "The chicken was the first bird to have its genome sequenced, in 2004.",
-    "Baby chickens are chicks. Female chickens are pullets until they’re old enough to lay eggs and become hens. Male chickens are called roosters, cocks or cockerels, depending on the country you’re in"
-]
-
-const niceMessage = () => {
-    const mId = Math.floor(Math.random() * Math.floor(8));
-    return messages[mId];
-}
-
-app.get('/canta-galo', (req, res) => res.send(shouldCanta()? "canta" : "cala"))
-
-app.post('/canta-galo', (req, res) => {
-    cantaGalo()
-    if (logEnabled) {
-        console.log("req.headers", req.headers);
-        console.log("req.params", req.params);
-        console.log("req.body", req.body);
-    }
-    res.send(200, niceMessage())
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}! [LOG=${logEnabled}]`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! [LOG=${process.env["LOG"]}]`))
