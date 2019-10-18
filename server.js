f97ff965f21878a9eed15ee1dca2591840c21e96
Marcelo Garcia <margarcia@zendesk.com>
2019-10-18 13:30:46 +1100

    add https

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 const galo = require('./galo')
 const app = express()
 
@@ -6,8 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env["PORT"] || 8080
+const enable_http = !!process.env["ENABLE_HTTPS"]
 
 app.get('/canta-galo', galo.galoGET)
 app.post('/canta-galo', galo.galoPOST)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}! [LOG=${process.env["LOG"]}]`))
+if (enable_http) {
+    https.createServer({
+        key: fs.readFileSync('server.key'),
+        cert: fs.readFileSync('server.cert')
+    }, app).listen(port, () => {
+        console.log(`Canta Galo listening on HTTPS port ${port}! [LOG=${process.env["LOG"]}]`)
+      })
+} else {
+    app.listen(port, () => console.log(`Canta Galoo listening on port ${port}! [LOG=${process.env["LOG"]}]`))
+}
+
+
